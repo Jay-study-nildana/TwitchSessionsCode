4e6e7c0c039a52b2dd3746f1b26c00b7faa5ce60
Jay <vijayasimhabr@live.com>
2022-09-19 17:05:15 +0530

    session 3 ended
    today touched upon comments

---
+++ Session3/practicefile.js
@@ -0,0 +1,16 @@
+//https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/comment-your-javascript-code
+
+//let monkey = "monkey wants banana";
+let giraffe = "giraffe wants tall trees";
+
+//i am showing the value of the variable giraffe
+//and I am using the javascript console function.
+
+/* 
+look at this.
+this will be ignored. 
+this also will be ignored.
+i dont have to put // in every line
+becuase i am already commented.
+*/
+console.log(giraffe)
