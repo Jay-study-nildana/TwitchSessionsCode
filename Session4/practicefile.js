d2680ddf7c117b72894b23645d39063e236b103b
Jay <vijayasimhabr@live.com>
2022-09-21 20:54:45 +0530

    session 4 wrapped up
    declaring javascript variables
    
    assigning the value of one variable to another variable

---
+++ Session4/practicefile.js
@@ -0,0 +1,16 @@
+//https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/declare-javascript-variables
+
+//https://www.w3schools.com/Js/js_es6.asp
+
+var myName;
+
+//https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/assigning-the-value-of-one-variable-to-another
+
+var superHero = "Batman";
+var theDarkKnight = superHero;
+
+console.log(superHero);
+console.log(theDarkKnight);
+
+//next :
+//https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/basic-javascript/initializing-variables-with-the-assignment-operator 
